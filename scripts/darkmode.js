587064901fad8ff9c27f99d7a2eccad6b6e1997f
Vítor Silva Medeiros <87152620+vsmedeiros@users.noreply.github.com>
2025-02-28 09:47:14 -0300

    fix: remove debug console log from dark mode toggle function

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,5 +1,4 @@
 function toggleDarkMode() {
-  console.log("clicado");
   document.documentElement.classList.toggle("light-mode");
   const icon = document.querySelector(".dark-mode-toggle i");
   if (document.documentElement.classList.contains("light-mode")) {
