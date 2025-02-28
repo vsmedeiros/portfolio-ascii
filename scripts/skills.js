da9f1ece74cf27ff5ee26ccebabf0e55a303cfdf
Vítor Silva Medeiros <87152620+vsmedeiros@users.noreply.github.com>
2025-02-28 02:04:38 -0300

    feat: update portfolio sections with improved IDs and content. Add skills section with detailed ratings and restructure layout for better readability.

---
+++ scripts/skills.js
@@ -0,0 +1,13 @@
+const mediumRatingElements = document.getElementsByClassName("rating-medium");
+const initialRatingElements = document.getElementsByClassName("rating-initial");
+const highRatingElements = document.getElementsByClassName("rating-high");
+
+addStarRating(highRatingElements, 3);
+addStarRating(mediumRatingElements, 2);
+addStarRating(initialRatingElements, 1);
+
+function addStarRating(elements, rating) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = '<i class="fas fa-star"></i>'.repeat(rating);
+  }
+}
